f19a115f160e5af22941c96a8202a853bfa50e0d
mv940 <marianavalerio566@gmail.com>
2024-06-05 10:35:00 +0200

    First commit plot the simulation

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Hello")
-print("my world")
-print("again")
+## Program to creat ECG ##
+
+import neurokit2 as nk  # Load the package
+import matplotlib.pyplot as plt
+
+simulated_ecg = nk.ecg_simulate(duration:=8, sampling_rate:=200, method="daubechies")
+
+plt.plot(simulated_ecg)
+plt.show()
