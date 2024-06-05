20ce8bd093c8407c65437955a6de041983f844ad
mv940 <marianavalerio566@gmail.com>
2024-06-05 17:29:53 +0200

    Plot create a list and save correct now

--- main.py
+++ main.py
@@ -6,19 +6,24 @@ import openpyxl
 import numpy as np
 
 
-# Generate 60 seconds of ECG signal (recorded at 500 samples/second)
-simulated_ecg = nk.ecg_simulate(duration:=60, sampling_rate:=500, heart_rate:=70) #method="daubechies"
+# Generate 60 seconds of ECG signal (recorded at 500 samples/second) for human
+#simulated_ecg = nk.ecg_simulate(duration:=60, sampling_rate:=500, heart_rate:=70) #method="daubechies"
+
+#Generate for rats
+
+simulated_ecg = nk.ecg_simulate(duration:=60, sampling_rate:=500, frequency:=50)
+
+#FALTA VER A DISTANCIA ENTRE OS R, ENTAO POSSO COLOCAR ALGUMAS MARCACOES
 
 ## Monta grafico ##
 
-plt.plot(simulated_ecg )
+plt.plot(simulated_ecg)
 plt.show()
 
 ## Converter Array ##
 
 a = np.array(simulated_ecg)
 b = a.tolist() #Uma linha em lista
-
 
 ## Create a Excel ##
 
