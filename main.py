0637e367e49daf1e1ea2e425dd72f67f29afc0e0
mv940 <marianavalerio566@gmail.com>
2024-06-05 11:32:57 +0200

    Plot create a list and save

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 import neurokit2 as nk  # Load the package
 import matplotlib.pyplot as plt
+import openpyxl
+import pandas as pd
 
-simulated_ecg = nk.ecg_simulate(duration:=8, sampling_rate:=200, method="daubechies")
+#simulated_ecg = nk.ecg_simulate(duration:=8, sampling_rate:=200, method="daubechies")
+simulated_ecg = 1
 
 plt.plot(simulated_ecg)
 plt.show()
@@ -17,3 +20,12 @@ print(main_list)
 #print(main_list[0])
 
 ## Create a Excel ##
+
+workbook = openpyxl.Workbook()
+sheet = workbook.active
+data = [main_list]
+for row in data:
+    sheet.append(row)
+
+workbook.save("test_ECG_One.xlsx")
+print("Excel file created")
