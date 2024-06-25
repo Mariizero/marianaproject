43082b71b38126c958a0ae40ce189a8b3144dff5
mv940 <marianavalerio566@gmail.com>
2024-06-25 10:45:35 +0200

    Plot grafico sem filtro

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 ## Program to creat ECG ##
 
-import neurokit2 as nk  # Load the package
+
 import matplotlib.pyplot as plt
 import openpyxl
 import numpy as np
-
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 import pandas as pd
 import plotly.graph_objs as go
 
@@ -19,83 +20,43 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 
+# Função para abrir a caixa de diálogo e selecionar o arquivo Excel
+def selecionar_arquivo():
+    Tk().withdraw()  # Ocultar a janela principal do Tkinter
+    arquivo_selecionado = askopenfilename(
+        title="Selecione o arquivo Excel",
+        filetypes=[("Arquivo Excel", "*.xlsx *.xls")]
+    )
+    return arquivo_selecionado
 
-def Calcular_ECG(self):
+caminho_do_arquivo = selecionar_arquivo()
 
-    # Generate TEM Q ABRIR um arquivo em EXCEL e pegar apenas a primeira coluna.
+# Verificar se um arquivo foi selecionado
+if caminho_do_arquivo:
+    # Carregar o arquivo Excel, especificando que queremos apenas a primeira coluna
+    df = pd.read_excel(caminho_do_arquivo, usecols=[0])
+
+    dados = df[1:2000]
+
+    print(dados)
 
 
 
-
-
-
-    fs = 140  # Distance RR
-    duration = 5  # Seconds
-    t, simulated_ecg = generate_simulated_ecg(self, fs, duration)
-
-    # simulated_ecg = nk.ecg_simulate(duration:=60, sampling_rate:=500, heart_rate:=70)
-
-    # Find R-peaks
-    peaks, _ = find_peaks(simulated_ecg, distance=100, height=0.5)
-
-    # Plot the ECG signal with detected R-peaks
-    plt.figure(figsize=(12, 4))
-    plt.plot(simulated_ecg)
-    plt.plot(peaks, simulated_ecg[peaks], "x", color='red', markersize=10)
-    plt.title('Simulated ECG Signal with R-peaks Detected')
-    plt.xlabel('Sample milisec')
-    plt.ylabel('Amplitude')
-    plt.grid(True)
+    plt.plot(dados)
+    plt.title('Unfiltered ECG Signal Lead I')
+    plt.xlabel('Time in ms')
+    plt.ylabel('Voltage in mV')
     plt.show()
 
-    # Print the indices of detected R-peaks
-    print("Indices of R-peaks:", peaks)
-
-    # Save ECG data and R-peak indices to an Excel file
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-
-    # Add headers
-    sheet["A1"] = "Time (s)"
-    sheet["B1"] = "ECG Signal"
-    sheet["C1"] = "R-peak Indices"
-
-    # Add ECG signal and time
-    for i, (time, ecg_value) in enumerate(zip(t, simulated_ecg), start=2):
-        sheet.cell(row=i, column=1, value=time)
-        sheet.cell(row=i, column=2, value=ecg_value)
-
-    # Add R-peak indices
-    for i, peak in enumerate(peaks, start=2):
-        sheet.cell(row=i, column=3, value=peak)
-
-    workbook.save("test_ECG_One.xlsx")
-    print("Excel file created :)")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    #print(df.to_string(index=False))
+else:
+    print("Nenhum arquivo foi selecionado.")
 
 
 
 # Generate for rats
 
-class MainWindow(Screen):
-
+"""class MainWindow(Screen):
 
 
     def Calcular_ECG(self): #COMO VAI CHAMAR UMA VARIAVEL DE OUTRA FUNCAO
@@ -156,7 +117,7 @@ class ECG(App):  #Determina o nome do aplicativo
         return kv
 
 if __name__ == "__main__":
-    ECG().run()
+    ECG().run()"""
 
 
 
